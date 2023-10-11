124e7a6c8856a134ede6c6615d38946f5d4763ff
HeoJongWoon <whddns0902@gmail.com>
2023-10-12 05:02:26 +0900

    Department, Youtube 스타일주기 과정

---
+++ src/components/sub/department/Department.jsx
@@ -0,0 +1,59 @@
+import Layout from '../../common/layout/Layout';
+import { useEffect, useState } from 'react';
+import './Department.scss';
+const path = process.env.PUBLIC_URL;
+//1.useEffect로 컴포넌트 마운트되자마자 fetch외부데이터 가져옴
+//2.데이터가 다 받아지면 useState로 state에 해당 담아줌
+//3.return문 안족에 state값을 map으로 반복돌면서 JSX출력
+
+export default function Department() {
+	const [Department, setDepartment] = useState([]);
+
+	useEffect(() => {
+		fetch(`${path}/DB/department.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				setDepartment(json.members);
+			});
+	}, []);
+
+	return (
+		<Layout title={'Department'}>
+			<div className='topContainer'>
+				<div className='txtBox'>
+					<span>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non ut nam natus rerum? Ipsa adipisci totam nihil provident, ut perspiciatis praesentium
+						rem!
+					</span>
+					<div className='inContainer'>
+						<div className='infoBox'></div>
+						<div className='imgBox'>
+							<img src='img/Department.jpg' />
+						</div>
+					</div>
+				</div>
+			</div>
+			<h1>Our team</h1>
+			<div className='container'>
+				<div className='infoBox'></div>
+				<div className='memberBox'>
+					{Department.map((member, idx) => {
+						return (
+							<article key={idx}>
+								<div className='pic'>
+									<img src={`${path}/img/${member.pic}`} alt={member.name} />
+									<img src={`${path}/img/${member.pic}`} alt={member.name} />
+								</div>
+								<h2>{member.name}</h2>
+								<p>{member.position}</p>
+							</article>
+						);
+					})}
+				</div>
+			</div>
+			<div></div>
+			<h1 className='lastTit'>News</h1>
+			<div className='bottomContainer'></div>
+		</Layout>
+	);
+}
