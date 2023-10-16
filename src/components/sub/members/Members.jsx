65dde4a78b6fce2dd0bd9d999c9c930224301099
HeoJongWoon <whddns0902@gmail.com>
2023-10-16 10:23:55 +0900

    onChange이벤트시 실시간으로 state변경하면서 input요소에 출력하는 방법

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,7 +1,22 @@
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
+import { useState } from 'react';
 
 export default function Members() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		//현재 onChange이벤트가 발생하고 있는 form요소의 name값을 객체안에서 변수로 가져오고 value값도 가져온뒤 기존의 state값을 deep copy한뒤 내가 입력하고 있는 input의 property값만 덮어쓰기
+		setVal({ ...Val, [name]: value });
+	};
+
 	return (
 		<Layout title={'Members'}>
 			<form>
@@ -15,7 +30,7 @@ export default function Members() {
 									<label htmlFor='userid'>userid</label>
 								</th>
 								<td>
-									<input type='text' id='userid' name='userid' />
+									<input type='text' id='userid' name='userid' value={Val.userid} onChange={handleChange} />
 								</td>
 							</tr>
 
@@ -25,7 +40,7 @@ export default function Members() {
 									<label htmlFor='pwd1'>password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd1' name='pwd1' />
+									<input type='password' id='pwd1' name='pwd1' value={Val.pwd1} onChange={handleChange} />
 								</td>
 							</tr>
 
@@ -35,7 +50,24 @@ export default function Members() {
 									<label htmlFor='pwd2'>re-password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd2' name='pwd2' />
+									<input type='password' id='pwd2' name='pwd2' value={Val.pwd2} onChange={handleChange} />
+								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>e-mail</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										//onChange가 발생할때마다 실시간으로 변경되고 있는 state의 value값을 출력
+										value={Val.email}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
