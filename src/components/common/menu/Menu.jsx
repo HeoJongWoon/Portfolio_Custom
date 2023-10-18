d57027d0b9b6d905c2aa9eab42cc055593956900
HeoJongWoon <whddns0902@gmail.com>
2023-10-18 16:11:59 +0900

    framer-motoion으로 메뉴 슬라이드 모션 추가

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -1,46 +1,49 @@
 import './Menu.scss';
 import { Link, NavLink } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function Menu() {
 	return (
-		<aside className='Menu'>
-			<h1>
-				<Link to='/'>LOGO</Link>
-			</h1>
+		<AnimatePresence>
+			<motion.aside className='Menu' initial={{ x: '-100%' }} animate={{ x: '0%' }} exit={{ x: '-100%' }} transition={{ duration: 0.5 }}>
+				<h1>
+					<Link to='/'>LOGO</Link>
+				</h1>
 
-			<ul>
-				<li>
-					<NavLink to='/department' activeClassName='active'>
-						Department
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/community' activeClassName='active'>
-						Community
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/gallery' activeClassName='active'>
-						Gallery
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube' activeClassName='active'>
-						Youtube
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/members' activeClassName='active'>
-						Members
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/contact' activeClassName='active'>
-						Contact
-					</NavLink>
-				</li>
-			</ul>
-		</aside>
+				<ul>
+					<li>
+						<NavLink to='/department' activeClassName='active'>
+							Department
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeClassName='active'>
+							Community
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeClassName='active'>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeClassName='active'>
+							Youtube
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeClassName='active'>
+							Members
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/contact' activeClassName='active'>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+			</motion.aside>
+		</AnimatePresence>
 	);
 }
 
