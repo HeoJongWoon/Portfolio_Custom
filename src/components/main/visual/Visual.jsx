33aab85d356f0c87819e7acc7686d09369152fc2
HeoJongWoon <whddns0902@gmail.com>
2023-10-19 10:48:35 +0900

    기본 스와이퍼 패널 스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -8,12 +8,15 @@ function Visual() {
 	return (
 		<section className='visual'>
 			<h2>Visual</h2>
-			<Swiper>
+			<Swiper slidesPerView={3} spaceBetween={50}>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
 							<div className='inner'>
+								<div className='pic'>
+									<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
+								</div>
 								<h2>{vid.snippet.title}</h2>
 							</div>
 						</SwiperSlide>
