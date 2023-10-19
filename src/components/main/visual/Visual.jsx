e9bf053ca279b75bdfcdc1b76e03095e1f0f3da9
HeoJongWoon <whddns0902@gmail.com>
2023-10-19 11:38:03 +0900

    swiper 모양 커스터마이징

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -5,19 +5,18 @@ import 'swiper/css';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
+	console.log(data);
+
 	return (
 		<section className='visual'>
-			<h2>Visual</h2>
-			<Swiper slidesPerView={3} spaceBetween={50}>
+			<Swiper slidesPerView={3} spaceBetween={50} loop={true} centeredSlides={true}>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
-							<div className='inner'>
-								<div className='pic'>
-									<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
-								</div>
-								<h2>{vid.snippet.title}</h2>
+							<div className='pic'>
+								<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
+								<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
 							</div>
 						</SwiperSlide>
 					);
