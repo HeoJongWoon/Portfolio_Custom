4f69786f628d3205b4f11177122eb8694d740c49
HeoJongWoon <whddns0902@gmail.com>
2023-10-19 16:08:27 +0900

    브라우저 리사이즈시 세로 위치값 계속 갱신처리

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 			<Route path='/contact' component={Contact} />
 			<Route path='/community' component={Community} />
 			<Route path='/detail/:id' component={Detail} />
-			{/* <Route path='/' component={Footer} /> */}
+			{/* <Footer /> */}
 			<Menu />
 		</main>
 	);
